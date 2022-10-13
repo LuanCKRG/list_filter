197164dbbbbcec6fcde314d63ece9fb343d415f8
Luan V <crackersage@gmail.com>
2022-10-13 13:42:50 -0400

    Initial commit

---
+++ lib/list_filter.ex
@@ -0,0 +1,11 @@
+defmodule ListFilter do
+  def call(list) do
+    list
+    |> Enum.filter(fn elem -> is_number(elem) end)
+    |> Enum.count(fn num -> is_odd(num) end)
+  end
+
+  defp is_odd(num) do
+    rem(num, 2) != 0
+  end
+end
